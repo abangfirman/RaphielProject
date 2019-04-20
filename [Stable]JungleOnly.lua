a9002f1c9da908cc2e3cf50db137d4ae9d6e9b05
Taiyouscape <43681888+Taiyouscape@users.noreply.github.com>
2019-04-20 21:29:01 +0700

    Update [Stable]JungleOnly.lua

--- [Stable]JungleOnly.lua
+++ [Stable]JungleOnly.lua
@@ -9,7 +9,7 @@ end
 goto START
 
 ::START::
-menu = gg.choice ({'[1️⃣]Ｒａｄａｒ 太陽','[2️⃣]Ｄｒｏｎｅ 太陽','[⚠️]Ｃｒｅｄｉｔ','[⛔]Ｅｘｉｔ'}, nill, "The version you have is Taiyou Project Script v1.2stable1204190703jungleonly")
+menu = gg.choice ({'[1️⃣]Ｒａｄａｒ 太陽','[2️⃣]Ｄｒｏｎｅ 太陽','[⚠️]Ｃｒｅｄｉｔ','[⛔]Ｅｘｉｔ'}, nill, "The version you have is Taiyou Project Script v1.2stable2004192128normalonly")
 
 if menu == 1 then goto maplist end
 if menu == 2 then goto drone end
